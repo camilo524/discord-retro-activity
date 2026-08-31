754babdbd350de14c99f2853210fcbf5d955fc44
Camiloh <64754312+camilo524@users.noreply.github.com>
2026-08-31 18:56:33 -0500

    Update games.js

--- js/games.js
+++ js/games.js
@@ -60,16 +60,13 @@ const CONTROL_LAYOUTS = {
   //        A
   diamond: [
     { type: "dpad", location: "left", left: "12%", top: "55%", joystickInput: false, inputValues: [4, 5, 6, 7] },
-
-    // Arriba
-    { type: "button", text: "Y", id: "y", location: "right", left: 50, top: 10, bold: true, input_value: 9 },
-    // Izquierda
-    { type: "button", text: "X", id: "x", location: "right", left: 10, top: 50, bold: true, input_value: 1 },
-    // Derecha
-    { type: "button", text: "B", id: "b", location: "right", left: 90, top: 50, bold: true, input_value: 0 },
-    // Abajo
-    { type: "button", text: "A", id: "a", location: "right", left: 50, top: 90, bold: true, input_value: 8 },
-
+  
+    // Rombo más a la izquierda (right más alto en CSS se aplica aparte)
+    { type: "button", text: "Y", id: "y", location: "right", left: 45, top: 5, bold: true, input_value: 9 },
+    { type: "button", text: "X", id: "x", location: "right", left: 5, top: 45, bold: true, input_value: 1 },
+    { type: "button", text: "B", id: "b", location: "right", left: 85, top: 45, bold: true, input_value: 0 },
+    { type: "button", text: "A", id: "a", location: "right", left: 45, top: 85, bold: true, input_value: 8 },
+  
     { type: "button", text: "Start", id: "start", location: "center", left: 55, top: 40, fontSize: 13, block: true, input_value: 3 },
     { type: "button", text: "Select", id: "select", location: "center", left: -20, top: 40, fontSize: 13, block: true, input_value: 2 }
   ],
