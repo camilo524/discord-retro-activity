077cbe5fe2fa319defb1f5485db4d24b8c19fb00
Camiloh <64754312+camilo524@users.noreply.github.com>
2026-09-03 18:53:21 -0500

    Update games.js

--- js/games.js
+++ js/games.js
@@ -68,7 +68,7 @@ const GAMES = [
     name: "King Of Fighters 98",
     core: "arcade",
     rom: "https://files.camiloh.co/kof98.zip",
-    bios: "https://files.camiloh.co/neogeo2.zip",
+    bios: "https://files.camiloh.co/neogeo.zip",
     cover: "./covers/road-fighter.webp",
     sizeMB: 0.03,
     controls: "default"
