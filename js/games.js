c7abedac005869a420fc3ad4793ea60bf987d8fc
Camiloh <64754312+camilo524@users.noreply.github.com>
2026-09-03 17:10:16 -0500

    Update games.js

--- js/games.js
+++ js/games.js
@@ -10,7 +10,7 @@ const GAMES = [
     id: 1,
     name: "Contra",
     core: "nes",
-    rom: "contra.nes",
+    rom: "./roms/contra.nes",
     cover: "./covers/contra.webp",   // imagen optimizada
     sizeMB: 0.13,                    // peso aproximado en MB
     // layout: "default" | "diamond" | "nes"
