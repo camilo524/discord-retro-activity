99ede5fcb1b904f3bd3a132f79582bac00fb7000
Camiloh <64754312+camilo524@users.noreply.github.com>
2026-09-07 14:32:29 -0500

    Update games.js

--- js/games.js
+++ js/games.js
@@ -49,7 +49,7 @@ const GAMES = [
     id: 5,
     name: "Bloody Roar",
     core: "psx",
-    rom: "https://files.camiloh.co/bloar.zip",
+    rom: "https://files.camiloh.co/bloody-roar.zip",
     cover: "./covers/blody.webp",
     sizeMB: 277,
     controls: "diamond"              // rombo: Y X / B A
